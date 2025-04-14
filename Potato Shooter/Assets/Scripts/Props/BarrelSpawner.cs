6198553a28e01603ffcc9898d9664322ce499d00
Rozhina-N <549377@student.fontys.nl>
2025-04-14 17:14:16 +0200

    respawning barrels

--- Potato Shooter/Assets/Scripts/Props/BarrelSpawner.cs
+++ Potato Shooter/Assets/Scripts/Props/BarrelSpawner.cs
@@ -3,23 +3,29 @@ using UnityEngine;
 public class BarrelSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject barrelPrefab;
-    [SerializeField] private float spawnInterval = 2f;
+    [SerializeField] private float spawnInterval = 20f;
     [SerializeField] private float lastSpawnTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         lastSpawnTime = Time.time;
+        SpawnBarrel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time - lastSpawnTime >= 20f)
+        if (Time.time - lastSpawnTime >= spawnInterval)
         {
             SpawnBarrel();
             lastSpawnTime = Time.time;
         }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawCube(transform.position, new Vector3(1, 1, 1));
     }
 
     private void SpawnBarrel()
